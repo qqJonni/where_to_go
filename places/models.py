521cf5889489821bde7f2e464dee48521b9a54e6
Valeriy Lvov <tepliy_shov@mail.ru>
2023-08-22 00:51:50 +0500

    Add PlaceName model

--- places/models.py
+++ places/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class PlaceName(models.Model):
+    title = models.CharField(max_length=128, unique=True)
+    imgs = models.URLField(max_length=256)
+    description_short = models.TextField(null=True, blank=True)
+    description_long = models.TextField(null=True, blank=True)
+    coordinates = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
