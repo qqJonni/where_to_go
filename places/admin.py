7ebe353e504a0e174770bf5be48a71bd9abdd252
Valeriy Lvov <tepliy_shov@mail.ru>
2023-08-22 18:42:47 +0500

    Add model for images

--- places/admin.py
+++ places/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from places.models import PlaceName
+from places.models import PlaceName, Image
 
 admin.site.register(PlaceName)
+admin.site.register(Image)
--- places/models.py
+++ places/models.py
@@ -3,10 +3,17 @@ from django.db import models
 
 class PlaceName(models.Model):
     title = models.CharField(max_length=128, unique=True)
-    imgs = models.URLField(max_length=256)
     description_short = models.TextField(null=True, blank=True)
     description_long = models.TextField(null=True, blank=True)
     coordinates = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Image(models.Model):
+    title = models.CharField(max_length=128, unique=True)
+    imgs = models.ImageField(upload_to='images/', null=True, blank=True)
 
     def __str__(self):
         return self.title
